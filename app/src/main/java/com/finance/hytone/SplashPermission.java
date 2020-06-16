7dd57ce4654ef4d0ac355bcb2da82d0761aa1acd
sidpug <sidpug@users.noreply.github.com>
2020-06-16 14:29:45 +0530

    ui fix

--- app/src/main/java/com/finance/hytone/SplashPermission.java
+++ app/src/main/java/com/finance/hytone/SplashPermission.java
@@ -3,7 +3,6 @@ package com.finance.hytone;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -17,8 +16,7 @@ public class SplashPermission extends AppCompatActivity {
             startActivity(new Intent(SplashPermission.this, MainActivity.class));
             finish();
         }
-        Button nxt = findViewById(R.id.next);
-        nxt.setOnClickListener(new View.OnClickListener() {
+        findViewById(R.id.next).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 startActivity(new Intent(SplashPermission.this, SplashPermission2.class));
--- app/src/main/java/com/finance/hytone/SplashPermission2.java
+++ app/src/main/java/com/finance/hytone/SplashPermission2.java
@@ -9,6 +9,7 @@ import android.widget.CheckBox;
 import android.widget.CompoundButton;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class SplashPermission2 extends AppCompatActivity {
@@ -18,65 +19,51 @@ public class SplashPermission2 extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_permission2);
 
+        final Button accept = findViewById(R.id.allow);
+        CheckBox checkBox = findViewById(R.id.checkBox);
+        accept.setEnabled(false);
+        checkBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked) {
+                    accept.setEnabled(true);
+                } else
+                    accept.setEnabled(false);
 
+            }
+        });
 
-            final Button back = findViewById(R.id.back), accept = findViewById(R.id.allow);
-            CheckBox checkBox = findViewById(R.id.checkBox);
-            accept.setEnabled(false);
-            back.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    //startActivity(new Intent(SplashPermission2.this, SplashPermission.class));
-                    finish();
-                }
-            });
-            checkBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-                @Override
-                public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                    if (isChecked) {
-                        accept.setEnabled(true);
-                    } else
-                        accept.setEnabled(false);
+        accept.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Helper.putAccepted(SplashPermission2.this, true);
+                if (Permission.checkAllPermissions(SplashPermission2.this)) {
 
-                }
-            });
-
-            accept.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    Helper.putAccepted(SplashPermission2.this, true);
-                    if (Permission.checkAllPermissions(SplashPermission2.this)) {
-
-                        startActivity(new Intent(SplashPermission2.this, MainActivity.class));
-                    } else
-                        Permission.permission(SplashPermission2.this);
-                }
-            });
+                    startActivity(new Intent(SplashPermission2.this, MainActivity.class));
+                } else
+                    Permission.permission(SplashPermission2.this);
+            }
+        });
 
     }
 
     @Override
-    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
-        switch (requestCode) {
-            case 1: {
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if (requestCode == 1) {// If request is cancelled, the result arrays are empty.
+            if (Permission.checkAllPermissions(SplashPermission2.this)) {
 
-                // If request is cancelled, the result arrays are empty.
-                if (Permission.checkAllPermissions(SplashPermission2.this)) {
+                // permission was granted, yay! Do the
+                // contacts-related task you need to do.
+                Toast.makeText(SplashPermission2.this, "All permission GRANTED", Toast.LENGTH_SHORT).show();
+                Log.e("log_case2", "GRANTED!");
 
-                    // permission was granted, yay! Do the
-                    // contacts-related task you need to do.
-                    Toast.makeText(SplashPermission2.this, "All permission GRANTED", Toast.LENGTH_SHORT).show();
-                    Log.e("log_case2", "GRANTED!");
+            } else {
 
-                } else {
+                // permission denied, boo! Disable the
+                // functionality that depends on this permission.
+                Toast.makeText(SplashPermission2.this, "One or more Permissions denied", Toast.LENGTH_SHORT).show();
+                Log.e("log_case3", "NOT GRANTED!");
 
-                    // permission denied, boo! Disable the
-                    // functionality that depends on this permission.
-                    Toast.makeText(SplashPermission2.this, "One or more Permissions denied", Toast.LENGTH_SHORT).show();
-                    Log.e("log_case3", "NOT GRANTED!");
-
-                }
-                return;
             }
 
             // other 'case' lines to check for other
