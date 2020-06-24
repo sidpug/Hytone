fae32e3667fc44364ddf6709b5be78042602d06a
sidpug <sidpug@users.noreply.github.com>
2020-06-24 22:18:08 +0530

    Forms

--- app/src/main/java/com/finance/hytone/Form2.java
+++ app/src/main/java/com/finance/hytone/Form2.java
@@ -21,6 +21,7 @@ import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.util.Objects;
+import java.util.regex.Pattern;
 
 public class Form2 extends AppCompatActivity {
 
@@ -36,7 +37,7 @@ public class Form2 extends AppCompatActivity {
         findViewById(R.id.submit_form2).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if (handleSection1())
+                if (handleSection1() && aadhar() && pan() && voter_id() && dl() && passport() && bank())
                 {
                     submitForm();
                 }
@@ -69,7 +70,7 @@ public class Form2 extends AppCompatActivity {
         return true;
     }
 
-    public boolean handleSection2(){
+    public boolean address(){
         String address = Objects.requireNonNull(((TextInputEditText) findViewById(R.id.address)).getText()).toString().trim();
         String landmark = Objects.requireNonNull(((TextInputEditText) findViewById(R.id.landmark)).getText()).toString().trim();
         String ps = Objects.requireNonNull(((TextInputEditText) findViewById(R.id.ps)).getText()).toString().trim();
@@ -89,7 +90,7 @@ public class Form2 extends AppCompatActivity {
         return true;
     }
 
-    public boolean handleSection3(){
+    public boolean office_address(){
         String profession = Objects.requireNonNull(((TextInputEditText) findViewById(R.id.profession)).getText()).toString().trim();
         String mi = Objects.requireNonNull(((TextInputEditText) findViewById(R.id.mi)).getText()).toString().trim();
         String emi = Objects.requireNonNull(((TextInputEditText) findViewById(R.id.emi)).getText()).toString().trim();
@@ -99,6 +100,204 @@ public class Form2 extends AppCompatActivity {
         String cPs = Objects.requireNonNull(((TextInputEditText) findViewById(R.id.cPs)).getText()).toString().trim();
         String cPo = Objects.requireNonNull(((TextInputEditText) findViewById(R.id.cPo)).getText()).toString().trim();
         String cPincode = Objects.requireNonNull(((TextInputEditText) findViewById(R.id.cPincode)).getText()).toString().trim();
+
+        String error = null;
+        /*if (fName.length()==0)
+            error = "Please enter your First name";*/
+        //else if(eLname..)
+
+        if (error!=null)
+        {
+            Helper.showdialog(Form2.this, true, "", error);
+            return false;
+        }
+        return true;
+    }
+
+    public boolean aadhar() {
+        String aadhar_no = Objects.requireNonNull(((TextInputEditText) findViewById(R.id.aadhar_number)).getText()).toString().trim();
+        String error = null;
+        if (aadhar_no.length() == 0 || aadhar_no.length() < 12) {
+            ((TextInputEditText) findViewById(R.id.aadhar_number)).setError("Enter Valid Aadhar Number");
+            requestFocus(findViewById(R.id.aadhar_number));
+        }
+        ImageView front = findViewById(R.id.front_aadhar);
+        ImageView back = findViewById(R.id.back_aadhar);
+        findViewById(R.id.aadhar_f_button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                cameraIntent();//TODO: here we have to pass the image view id to camera intent
+            }
+        });
+        findViewById(R.id.aadhar_b_button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                cameraIntent(); // TODO: here we have to pass the image view id to camera intent
+            }
+        });
+
+        if (error!=null)
+        {
+            Helper.showdialog(Form2.this, true, "", error);
+            return false;
+        }
+        return true;
+    }
+
+    public boolean pan(){
+        String pan_no = Objects.requireNonNull(((TextInputEditText) findViewById(R.id.pan_number)).getText()).toString().trim();
+        String error = null;
+        if (pan_no.length() == 0 || pan_no.length() < 10)
+            ((TextInputEditText) findViewById(R.id.pan_number)).setError("Enter Valid Pan Number");
+
+        ImageView front = findViewById(R.id.front_pan);
+        ImageView back = findViewById(R.id.back_pan);
+        findViewById(R.id.pan_f_button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                cameraIntent();//TODO: here we have to pass the image view id to camera intent
+            }
+        });
+        findViewById(R.id.pan_b_button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                cameraIntent(); // TODO: here we have to pass the image view id to camera intent
+            }
+        });
+
+        if (error!=null)
+        {
+            Helper.showdialog(Form2.this, true, "", error);
+            return false;
+        }
+        return true;
+    }
+
+    public boolean voter_id(){
+        String voter_id_no = Objects.requireNonNull(((TextInputEditText) findViewById(R.id.voter_id_number)).getText()).toString().trim();
+        String error = null;
+        if (voter_id_no.length() == 0 )
+            ((TextInputEditText) findViewById(R.id.voter_id_number)).setError("Enter Valid Voter Id Number");
+
+        ImageView front = findViewById(R.id.front_voter_id);
+        ImageView back = findViewById(R.id.back_voter_id);
+        findViewById(R.id.voter_id_f_button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                cameraIntent();//TODO: here we have to pass the image view id to camera intent
+            }
+        });
+        findViewById(R.id.voter_id_b_button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                cameraIntent(); // TODO: here we have to pass the image view id to camera intent
+            }
+        });
+
+        if (error!=null)
+        {
+            Helper.showdialog(Form2.this, true, "", error);
+            return false;
+        }
+        return true;
+    }
+
+    public boolean dl(){
+        String dl_no = Objects.requireNonNull(((TextInputEditText) findViewById(R.id.dl_no)).getText()).toString().trim();
+        String error = null;
+        if (dl_no.length() == 0 )
+            ((TextInputEditText) findViewById(R.id.dl_no)).setError("Enter Valid Driving Licence Number");
+
+        ImageView front = findViewById(R.id.front_dl_no);
+        ImageView back = findViewById(R.id.back_dl_no);
+        findViewById(R.id.dl_no_f_button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                cameraIntent();//TODO: here we have to pass the image view id to camera intent
+            }
+        });
+        findViewById(R.id.dl_no_b_button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                cameraIntent(); // TODO: here we have to pass the image view id to camera intent
+            }
+        });
+
+        if (error!=null)
+        {
+            Helper.showdialog(Form2.this, true, "", error);
+            return false;
+        }
+        return true;
+    }
+
+    public boolean passport(){
+        String passport = Objects.requireNonNull(((TextInputEditText) findViewById(R.id.passport_number)).getText()).toString().trim();
+        String error = null;
+        if (passport.length() == 0 )
+            ((TextInputEditText) findViewById(R.id.passport_number)).setError("Enter Valid Passport Number");
+
+        ImageView front = findViewById(R.id.front_passport);
+        ImageView back = findViewById(R.id.back_passport);
+        findViewById(R.id.passport_f_button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                cameraIntent();//TODO: here we have to pass the image view id to camera intent
+            }
+        });
+        findViewById(R.id.passport_b_button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                cameraIntent(); // TODO: here we have to pass the image view id to camera intent
+            }
+        });
+
+        if (error!=null)
+        {
+            Helper.showdialog(Form2.this, true, "", error);
+            return false;
+        }
+        return true;
+    }
+
+    public boolean bank(){
+        String bankname = Objects.requireNonNull(((TextInputEditText) findViewById(R.id.bank_name)).getText()).toString().trim();
+        String acc_no = Objects.requireNonNull(((TextInputEditText) findViewById(R.id.acc_no)).getText()).toString().trim();
+        String ifsc = Objects.requireNonNull(((TextInputEditText) findViewById(R.id.ifsc_code)).getText()).toString().trim();
+        String error = null;
+        if (bankname.length() == 0 )
+            ((TextInputEditText) findViewById(R.id.bank_name)).setError("Enter Valid Bank Name");
+
+        if (acc_no.length() == 0 )
+            ((TextInputEditText) findViewById(R.id.acc_no)).setError("Enter Valid Account Number");
+
+        if (ifsc.length() == 0 )
+            ((TextInputEditText) findViewById(R.id.ifsc_code)).setError("Enter Valid Ifsc Code");
+
+        ImageView front = findViewById(R.id.statement);
+        findViewById(R.id.statement_button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                cameraIntent();//TODO: here we have to pass the image view id to camera intent
+            }
+        });
+
+
+        if (error!=null)
+        {
+            Helper.showdialog(Form2.this, true, "", error);
+            return false;
+        }
+        return true;
+    }
+
+    public boolean assets(){
+        String asset1 = Objects.requireNonNull(((TextInputEditText) findViewById(R.id.an1)).getText()).toString().trim();
+        String a_value1 = Objects.requireNonNull(((TextInputEditText) findViewById(R.id.av1)).getText()).toString().trim();
+        String asset2 = Objects.requireNonNull(((TextInputEditText) findViewById(R.id.an2)).getText()).toString().trim();
+        String a_value2 = Objects.requireNonNull(((TextInputEditText) findViewById(R.id.av2)).getText()).toString().trim();
+        String asset3 = Objects.requireNonNull(((TextInputEditText) findViewById(R.id.an3)).getText()).toString().trim();
+        String a_value3 = Objects.requireNonNull(((TextInputEditText) findViewById(R.id.av3)).getText()).toString().trim();
 
         String error = null;
         /*if (fName.length()==0)
@@ -127,6 +326,7 @@ public class Form2 extends AppCompatActivity {
             Toast.makeText(this, "Please provide write permissions from settings", Toast.LENGTH_SHORT).show();
         }
     }
+
     @Override
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
@@ -141,7 +341,9 @@ public class Form2 extends AppCompatActivity {
             }
 
     }
+
     int previewImgResId=-1;
+
     private void onCaptureImageResult(Intent data, Bitmap thumbnail) {
         currentPhotoPath = null;
         bit = thumbnail;
@@ -182,6 +384,7 @@ public class Form2 extends AppCompatActivity {
         Log.e("onacti1", "nothing");
         Log.e("onacti", "11");// + descimage);
     }
+
     public void requestFocus(View view)
     {
         if(view.requestFocus())
@@ -190,4 +393,23 @@ public class Form2 extends AppCompatActivity {
         }
     }
 
+    /*public boolean isNumeric(String strNum) {
+        if (strNum == null) {
+            return false;
+        }
+        try {
+            double d = Double.parseDouble(strNum);
+        } catch (NumberFormatException nfe) {
+            return false;
+        }
+        return true;
+    }*/
+    /*private Pattern pattern = Pattern.compile("-?\\\\d+(\\\\.\\\\d+)?");
+
+    public boolean isNumeric(String strNum) {
+        if (strNum == null) {
+            return false;
+        }
+        return pattern.matcher(strNum).matches();
+    }*/
 }
