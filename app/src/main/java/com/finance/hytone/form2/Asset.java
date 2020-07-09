3f1250bdc7e212e1cbdb3708d8000fbed3930fe9
Saurabh250 <65411015+Saurabh250@users.noreply.github.com>
2020-07-09 13:59:11 +0530

    Saurabh

---
+++ app/src/main/java/com/finance/hytone/form2/Asset.java
@@ -0,0 +1,18 @@
+package com.finance.hytone.form2;
+
+    import
+    import android.os.Bundle;
+
+    import androidx.appcompat.app.AppCompatActivity;
+
+    import com.finance.hytone.R;
+
+public class Asset extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.assets);
+    }
+}
+
