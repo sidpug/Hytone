cf0d0f13cbec8711dece668be682c8ff4e4ecdde
Saurabh250 <65411015+Saurabh250@users.noreply.github.com>
2020-07-09 14:12:13 +0530

    Saurabh

---
+++ app/src/main/java/com/finance/hytone/form2/VoterId.java
@@ -0,0 +1,15 @@
+package com.finance.hytone.form2;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.finance.hytone.R;
+
+public class VoterId extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.voter_id);
+    }
+}
