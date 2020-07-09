5b874c279e456e41893fa016a49ab136147a0856
Saurabh250 <65411015+Saurabh250@users.noreply.github.com>
2020-07-09 14:08:21 +0530

    Saurabh

---
+++ app/src/main/java/com/finance/hytone/form2/PanCard.java
@@ -0,0 +1,18 @@
+package com.finance.hytone.form2;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.finance.hytone.R;
+
+public class PanCard extends AppCompatActivity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.pan_card);
+    }
+}
+
+
